5867831dad4a6fecc4cabec961c6c8dc76d1567a
Natallia Viazenkina <natala.viaz@gmail.com>
2019-06-11 16:29:18 +0300

    Добавлены исходные данные для генерации объявлений

---
+++ js/main.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// константы
+var ADS_NUMBER = 8; // количество объявлений, которые необходимо сгенерировать
+
+// общие функции, необходимые для расчетов
+// создание числового массива заданной длины
+var getArrayOfNumbers = function (arrayLength) {
+  var arrayOfNumbers = [];
+  for (var i = 1; i <= arrayLength; i++) {
+    arrayOfNumbers.push(i);
+  }
+
+  return arrayOfNumbers;
+};
+
+// перемешивание массива
+var getMixedArray = function (arr) {
+  for (var i = arr.length - 1; i > 0; i--) {
+    // получаем индекс случайного элемента в массиве с длинной (i + 1),
+    // на первой итерации длина массива равна длине исходного,
+    // с каждой последующей - на единицу меньше
+    var randomIndex = Math.floor(Math.random() * (i + 1));
+
+    // меняем элементы местами
+    var temp = arr[i];
+    arr[i] = arr[randomIndex]; // случайно выбранный элемент перенесен в конец массива
+    arr[randomIndex] = temp; // на место случайно выбранного элемента записан элемент с индексом i
+  }
+
+  return arr;
+};
+
+// генерирование случайного элемента массива
+var getRandomArrayItem = function (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+// генерирование случайного числа из диапазона
+var getRandomNumber = function (min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+};
+
+// DOM-объекты
+var map = document.querySelector('.map');
+
+// исходные денные для генерации объявлений
+// тип недвижимости
+var offerTypes = ['palace', 'flat', 'house', 'bungalo'];
+
+// координата Y метки на карте, диапазон
+var locationY = {min: 130, max: 630};
+
+// координата X метки на карте, диапазон
+var locationX = {min: 0, max: map.offsetWidth};
+
+// массив чисел для генерации адреса аватара
+var avatarNumbers = getMixedArray(getArrayOfNumbers(ADS_NUMBER));
