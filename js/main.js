4a5be946950a4d6e3634d59b2552ae7c66bb27df
Natallia Viazenkina <natala.viaz@gmail.com>
2019-06-14 18:58:15 +0300

    Комментарии в формате jsdoc

--- js/main.js
+++ js/main.js
@@ -11,43 +11,71 @@ var mapPinTemplate = document.querySelector('#pin') // шаблон метки
   .querySelector('button');
 
 
-// количество объявлений, которые необходимо сгенерировать
+/**
+* количество объявлений, которые необходимо сгенерировать
+* @const
+* @type {number}
+*/
 var ADS_NUMBER = 8;
 
-// размеры пина из CSS
+/**
+* размеры пина из CSS
+* @const
+* @type {number}
+*/
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
 
-// исходные денные для генерации объявлений
-// тип недвижимости
+
+/**
+* тип недвижимости
+* @enum {string}
+*/
 var OFFER_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 
-// координата Y метки на карте, диапазон
-var locationY = {
+/**
+* координата Y метки на карте, диапазон
+* @enum {number}
+*/
+var LocationY = {
   MIN: 130,
   MAX: 630
 };
 
-// координата X метки на карте, диапазон
-var locationX = {
-  min: 0 + PIN_WIDTH / 2,
-  max: map.offsetWidth - PIN_WIDTH / 2
+/**
+* координата X метки на карте, диапазон
+* @enum {number}
+*/
+var LocationX = {
+  MIN: 0 + PIN_WIDTH / 2,
+  MAX: map.offsetWidth - PIN_WIDTH / 2
 };
 
 
-// общие функции, необходимые для расчетов
-// генерирование случайного элемента массива
+/**
+* генерируем случайный элемент массива
+* @param {array} arr
+* @return {(number|string|boolean|Array|Object)} Возвращает случайный элемент массива
+*/
 var getRandomArrayItem = function (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
-// генерирование случайного числа из диапазона
+/**
+* генерируем случайное число из диапазона
+* @param {number} min минимальное значение
+* @param {number} max максимальное значение
+* @return {number} Возвращает случайный элемент массива
+*/
 var getRandomNumber = function (min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
 };
 
-
-// создадим случайное объявление
+/**
+* создает случайное объявление
+* @param {number} numericalItem
+* @return {Object}
+*/
 var generateAd = function (numericalItem) {
   return {
     'author': {
@@ -57,8 +85,8 @@ var generateAd = function (numericalItem) {
       'type': getRandomArrayItem(OFFER_TYPES)
     },
     'location': {
-      'x': getRandomNumber(locationX.min, locationX.max),
-      'y': getRandomNumber(locationY.MIN, locationY.MAX)
+      'x': getRandomNumber(LocationX.MIN, LocationX.MAX),
+      'y': getRandomNumber(LocationY.MIN, LocationY.MAX)
     }
   };
 };
@@ -70,7 +98,11 @@ for (var i = 0; i < ADS_NUMBER; i++) {
   randomAdsList.push(generateAd(i + 1));
 }
 
-// используя шаблон создадим новый DOM-элемент для метки для нового объявления
+/**
+* используя шаблон создает  новый DOM-элемент для метки
+* @param {Object} obj JS-объект на основе которого происходит наполнение шаблона
+* @return {Element}
+*/
 var getNewPin = function (obj) {
   var newPin = mapPinTemplate.cloneNode(true);
   newPin.style.left = obj.location.x - PIN_WIDTH / 2 + 'px';
@@ -83,7 +115,10 @@ var getNewPin = function (obj) {
   return newPin;
 };
 
-// создадим необходимое количество меток для объявлений и добавим их в разметку
+/**
+* добавляет в разметку необходимое количество DOM-элементов
+* @return {Element}
+*/
 var getNewPinList = function () {
   var fragment = document.createDocumentFragment();
   for (var j = 0; j < ADS_NUMBER; j++) {
