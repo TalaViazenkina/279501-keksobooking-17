885fa5c0cd9e5fee506128647b5513dba103fee1
Natallia Viazenkina <natala.viaz@gmail.com>
2019-06-27 00:04:34 +0300

    Добавлен модуль обмена данными с сервером

---
+++ js/backend.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// модуль для обмена данными с сервером
+(function () {
+  var URL = 'https://js.dump.academy/keksobooking/data';
+
+  window.backend = {
+    load: function (onSuccess, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+      xhr.timeout = 30000; // 30s
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onSuccess(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + ' мс');
+      });
+
+      xhr.open('GET', URL);
+      xhr.send();
+    }
+  };
+})();
