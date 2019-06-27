a4339f37dd2519ed2d7faabf8d2210e7f5655f1f
Natallia Viazenkina <natala.viaz@gmail.com>
2019-06-27 20:38:49 +0300

    Реализовано закрытие сообщения об ошибке

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,13 @@
 'use strict';
 
 (function () {
+  /**
+  * @const
+  * @type {number}
+  */
+  var ESC_KEYCODE = 27;
+
+  // шаблон сообщения об ошибке
   var errorTemplate = document.querySelector('#error')
   .content
   .querySelector('.error');
@@ -46,12 +53,61 @@
       el.removeAttribute('disabled');
     },
 
+    /**
+    * ввыводит сообщение об ошибке
+    * @param {string} message
+    */
     onError: function (message) {
-      var errorNode = errorTemplate.cloneNode(true);
+      var errorNode = errorTemplate.cloneNode(true); // клонируем шаблон
       var errorText = errorNode.querySelector('.error__message');
-      errorText.textContent = message;
+      errorText.textContent = message; // добавляем текст сообщения
 
+      // добавляем в разметку
       main.insertAdjacentElement('afterbegin', errorNode);
+
+      /*
+      * удаляет сообщение об ошибке из DOM
+      * @param {event} evt
+      */
+      var closeError = function (evt) {
+        evt.preventDefault();
+        errorNode.remove();
+      };
+
+      /**
+      * закрывает сообщение по клику на кнопку
+      * @param {event} evt
+      */
+      var onButtonClick = function (evt) {
+        closeError(evt);
+        errorButton.removeEventListener('click', onButtonClick);
+      };
+
+      /**
+      * закрывает сообщение по клику на произвольную область
+      * @param {event} evt
+      */
+      var onDocumentMousedown = function (evt) {
+        closeError(evt);
+        document.removeEventListener('click', onDocumentMousedown);
+      };
+
+      /**
+      * закрывает сообщение по esc
+      * @param {event} evt
+      */
+      var onDocumentKeydown = function (evt) {
+        if (evt.keyCode === ESC_KEYCODE) {
+          closeError(evt);
+          document.removeEventListener('keydown', onDocumentKeydown);
+        }
+      };
+
+      var errorButton = errorNode.querySelector('.error__button');
+      // добавим обработчики событий для закрытия окна ошибки
+      errorButton.addEventListener('click', onButtonClick);
+      document.addEventListener('mousedown', onDocumentMousedown);
+      document.addEventListener('keydown', onDocumentKeydown);
     }
   };
 })();
