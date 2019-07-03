e481b7d032829aacceded7445353ed8f7ed35d32
Natallia Viazenkina <natala.viaz@gmail.com>
2019-07-03 17:12:17 +0300

    Реализовано 'устранение дребезга' в отрисовке меток при частой смене фильтров

--- js/filters.js
+++ js/filters.js
@@ -9,12 +9,23 @@
   var selectType = window.filters.mapFilters.querySelector('select[name=housing-type]'); // фильтр типа жилья
   var selectPrice = window.filters.mapFilters.querySelector('select[name=housing-price]'); // фильтр стоимости
 
-  var unselectedValue = 'any'; // значение "невыбранного" фильтра
-
   // переменные для хранения текущего значения выбранного фильтра
   // зададим "стартовое" значение
   var selectedTypeValue = selectType.value;
   var selectedPriceValue = selectPrice.value;
+
+  var unselectedValue = 'any'; // значение "невыбранного" фильтра
+
+
+  // массив для хранения отфильтрованного списка объявлений
+  var filteredData = [];
+
+  /**
+  * запускает отрисовку отфильтрованных объявлений
+  */
+  var updateAdsList = function () {
+    window.pin.getNewPinList(filteredData.slice(0, window.data.ADS_MAX_NUMBER));
+  };
 
 
   // объект-мапа для хранения зависимости выбранного фильтра и функции, изменяющей соответствующую переменную
@@ -46,7 +57,11 @@
   // создадим функции сравнения отдельных параметров объявления с выбранными фильтрами
   // потом на основе их соберем общий фильтр;
 
-  // по типу жилья
+  /**
+  * сравнивает тип жилья
+  * @param {Object} it
+  * @return {boolean}
+  */
   var checkTypeValue = function (it) {
     if (selectedTypeValue === unselectedValue) {
       return true;
@@ -54,7 +69,11 @@
     return it.offer.type === selectedTypeValue;
   };
 
-  // по стоимости жилья
+    /**
+  * сравнивает стоимость жилья
+  * @param {Object} it
+  * @return {boolean}
+  */
   var checkPriceValue = function (it) {
     if (selectedPriceValue === unselectedValue) {
       return true;
@@ -62,18 +81,23 @@
       return it.offer.price >= priceNumberMap.hight;
     }
     return it.offer.price >= priceNumberMap[selectedPriceValue].min
-    && it.offer.price >= priceNumberMap[selectedPriceValue].max;
+    && it.offer.price < priceNumberMap[selectedPriceValue].max;
   };
 
+
+  // добавим обработчик события change на всю форму,
+  // а выбранный фильтр будем отслеживать по evt.target
   window.filters.mapFilters.addEventListener('change', function (evt) {
+    // запишем значение выбранного фильтра в соответствующую переменную
     nameSelectedValueMap[evt.target.name](evt.target.value);
 
-    var filteredData = window.data.adsList.filter(function (ad) {
+    // отфильтруем данные
+    filteredData = window.data.adsList.filter(function (ad) {
       return checkTypeValue(ad) && checkPriceValue(ad);
     });
 
-
-    window.pin.getNewPinList(filteredData.slice(0, window.data.ADS_MAX_NUMBER));
+    // запустим отрисовку отфильтрованных меток
+    window.debounce(updateAdsList);
   });
 
 })();
