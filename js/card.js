8d55b6ca857a94c42fd18e63e4694c95eff2f034
Natallia Viazenkina <natala.viaz@gmail.com>
2019-07-08 18:03:44 +0300

    Функция renderCard() разделена на функции, отрисовывающие отдельные поля карточки

--- js/card.js
+++ js/card.js
@@ -17,6 +17,7 @@
     'palace': 'Дворец'
   };
 
+
   /**
   * заполняет поле текстовой информацией
   * @param {HTMLElement} field
@@ -31,6 +32,117 @@
     }
     return field.textContent;
   };
+
+  /**
+  * заполняет поле с ценой
+  * @param {HTMLElement} field
+  * @param {number} number
+  */
+  var fillPrice = function (field, number) {
+    field.textContent = fillField(field, number) + '₽/ночь';
+  };
+
+  /**
+  * заполняет поле с типом жилья
+  * @param {HTMLElement} field
+  * @param {string} text
+  */
+  var fillType = function (field, text) {
+    field.textContent = typeNameMap[fillField(field, text)];
+  };
+
+  /**
+  * заполняет поле с количеством комнат и гостей
+  * @param {HTMLElement} field
+  * @param {number} room
+  * @param {number} guest
+  */
+  var fillCapacity = function (field, room, guest) {
+    // добавим соответствие окончания числительному
+    var roomMessage;
+    if (room === 1 || (room > 20 && room % 10 === 1)) {
+      roomMessage = ' комната для ';
+    } else if (room > 1 && room < 5 || (room > 20 && (room % 10 > 1) && (room % 10 < 5))) {
+      roomMessage = ' комнаты для ';
+    } else {
+      roomMessage = ' комнат для ';
+    }
+    var guestMessage = (guest === 1) ? ' гостя' : ' гостей';
+
+    field.textContent = fillField(field, room) + roomMessage + fillField(field, guest) + guestMessage;
+  };
+
+  /**
+  * заполняет поле со временем заезда и выезда
+  * @param {HTMLElement} field
+  * @param {string} timeIn
+  * @param {string} timeOut
+  */
+  var fillTime = function (field, timeIn, timeOut) {
+    field.textContent = 'Заезд после ' + fillField(field, timeIn) + ', выезд до ' + fillField(field, timeOut);
+  };
+
+  /**
+  * заполняет поле со списком удобств
+  * @param {Element} parentNode
+  * @param {string} selector
+  * @param {Array} arr
+  */
+  var fillFeatures = function (parentNode, selector, arr) {
+    // удаляем все элементы, описывающие удобства, привнесенные из шаблона
+    window.utils.deleteNodeList(parentNode, selector);
+
+    // добавим только те удобства, что есть в объявлении
+    if (arr && arr.length > 0) {
+      arr.forEach(function (it) {
+        var feature = document.createElement('li');
+        feature.classList.add('popup__feature');
+        feature.classList.add('popup__feature--' + it);
+        parentNode.appendChild(feature);
+      });
+    } else {
+      parentNode.style.display = 'none';
+    }
+  };
+
+  /**
+  * заполняет блок с фотографиями на основании массива с адресами фотографий
+  * @param {Array} arr
+  * @param {Element} el
+  * @param {Element} parentEl
+  */
+  var fillPhoto = function (arr, el, parentEl) {
+    if (arr && arr.length > 0) {
+      var fragment = document.createDocumentFragment();
+      arr.forEach(function (it, i) {
+        if (i === 0) {
+          el.src = it;
+        } else {
+          el = el.cloneNode(true);
+          el.src = it;
+          fragment.appendChild(el);
+        }
+      });
+
+      parentEl.appendChild(fragment);
+    } else {
+      parentEl.style.display = 'none';
+    }
+  };
+
+  /**
+  * добавляет аватарку автора
+  * @param {HTMLElement} field
+  * @param {string} source
+  */
+  var fillAvatar = function (field, source) {
+    if (source) {
+      field.src = source;
+    } else {
+      field.style.display = 'none';
+    }
+  };
+
 
   /**
   * закрывает попап
@@ -58,6 +170,7 @@
     }
   };
 
+
   /**
   * отрисовывает карточку объявления по шаблону
   * @param {object} obj
@@ -68,115 +181,59 @@
     // копируем шаблон
     card = cardTemplate.cloneNode(true);
 
-    // заполняем карточку
     // поле с заголовком
     var cardTitle = card.querySelector('.popup__title');
-    fillField(cardTitle, obj.offer.title);
-
     // поле с адресом
     var cardAddress = card.querySelector('.popup__text--address');
-    fillField(cardAddress, obj.offer.address);
-
     // поле с ценой;
     var cardPrice = card.querySelector('.popup__text--price');
-    cardPrice.textContent = fillField(cardPrice, obj.offer.price) + '₽/ночь';
-
     // поле с типом жилья
     var cardType = card.querySelector('.popup__type');
-    cardType.textContent = typeNameMap[fillField(cardType, obj.offer.type)];
-
     // поле с количеством гостей и комнат
     var cardCapacity = card.querySelector('.popup__text--capacity');
-    // добавим соответствие окончания числительному
-    var rooms;
-    if (obj.offer.rooms === 1 || (obj.offer.rooms > 20 && obj.offer.rooms % 10 === 1)) {
-      rooms = ' комната для ';
-    } else if (obj.offer.rooms > 1 && obj.offer.rooms < 5 || (obj.offer.rooms > 20 && (obj.offer.rooms % 10 > 1) && (obj.offer.rooms % 10 < 5))) {
-      rooms = ' комнаты для ';
-    } else {
-      rooms = ' комнат для ';
-    }
-    var guests = (obj.offer.guests === 1) ? ' гостя' : ' гостей';
-
-    cardCapacity.textContent = fillField(cardCapacity, obj.offer.rooms) + rooms + fillField(cardCapacity, obj.offer.guests) + guests;
-
-    // поле с указанием времени заезда и выезда
-    var cardTime = card.querySelector('.popup__text--time');
-    cardTime.textContent = 'Заезд после ' + fillField(cardTime, obj.offer.checkin) + ', выезд до ' + fillField(cardTime, obj.offer.checkout);
-
     // поле с описанием
     var cardDescription = card.querySelector('.popup__description');
-    fillField(cardDescription, obj.offer.description);
-
-    //  поле с доступными удобствами
+    // поле с указанием времени заезда и выезда
+    var cardTime = card.querySelector('.popup__text--time');
+    // поле со списком доступных удобств
     var cardFeatures = card.querySelector('ul.popup__features');
-    /**
-    * заполняет блок с доступными удобствами
-    */
-    var fillFeatures = function () {
-      // удаляем все элементы, описывающие удобства, из шаблона
-      window.utils.deleteNodeList(cardFeatures, '.popup__feature');
-
-      // добавим только те удобства, что есть в объявлении
-      if (obj.offer.features && obj.offer.features.length > 0) {
-        obj.offer.features.forEach(function (it) {
-          var feature = document.createElement('li');
-          feature.classList.add('popup__feature');
-          feature.classList.add('popup__feature--' + it);
-          cardFeatures.appendChild(feature);
-        });
-      } else {
-        cardFeatures.style.display = 'none';
-      }
-    };
-
-    fillFeatures();
-
     // блок с фото
     var photosList = card.querySelector('.popup__photos');
     var photo = photosList.querySelector('.popup__photo');
-
-    if (obj.offer.photos && obj.offer.photos.length > 0) {
-      var fragment = document.createDocumentFragment();
-      obj.offer.photos.forEach(function (it, i) {
-        if (i === 0) {
-          photo.src = it;
-        } else {
-          photo = photo.cloneNode(true);
-          photo.src = it;
-          fragment.appendChild(photo);
-        }
-      });
-
-      photosList.appendChild(fragment);
-    } else {
-      photosList.style.display = 'none';
-    }
-
     // блок с аватаркой
     var cardAvatar = card.querySelector('.popup__avatar');
-    if (obj.author.avatar) {
-      cardAvatar.src = obj.author.avatar;
-    } else {
-      cardAvatar.style.display = 'none';
-    }
+
+    // кнопка закрытия
+    var cardButton = card.querySelector('.popup__close');
+
+
+    // заполняем карточку
+    fillField(cardTitle, obj.offer.title); // заголовок
+    fillField(cardAddress, obj.offer.address); // адрес
+    fillPrice(cardPrice, obj.offer.price); // цена
+    fillType(cardType, obj.offer.type); // тип жилья
+    fillCapacity(cardCapacity, obj.offer.rooms, obj.offer.guests); // количество комнат и гостей
+    fillTime(cardTime, obj.offer.checkin, obj.offer.checkout); // время заезда/выезда
+    fillField(cardDescription, obj.offer.description); // описание
+    fillFeatures(cardFeatures, '.popup__feature', obj.offer.features); // удобства
+    fillPhoto(obj.offer.photos, photo, photosList); // фото
+    fillAvatar(cardAvatar, obj.author.avatar);
+
 
     // закрытие попапа
-    var cardButton = card.querySelector('.popup__close'); // кнопка закрытия
-
     cardButton.addEventListener('click', function (evt) {
       evt.preventDefault();
       closePopap();
     });
-
     document.addEventListener('keydown', onPopapEscPress);
+
 
     // добавляем отрисованную карточку в разметку
     window.data.MAP.insertBefore(card, window.data.MAP.querySelector('.map__filters-container'));
   };
 
   window.card = {
-    renderCard: renderCard,
-    removeCard: removeCard
+    render: renderCard,
+    remove: removeCard
   };
 })();
