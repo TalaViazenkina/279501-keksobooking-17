7ff70b3833505edc38f6bcc194c70f1a338f3fc5
Natallia Viazenkina <natala.viaz@gmail.com>
2019-07-09 17:49:19 +0300

    Добавлены фильтры количества комнат и гостей

--- js/filters.js
+++ js/filters.js
@@ -8,11 +8,15 @@
 
   var selectType = window.filters.form.querySelector('select[name=housing-type]'); // фильтр типа жилья
   var selectPrice = window.filters.form.querySelector('select[name=housing-price]'); // фильтр стоимости
+  var selectRoom = window.filters.form.querySelector('select[name=housing-rooms]'); // фильтр количества комнат
+  var selectGuest = window.filters.form.querySelector('select[name=housing-guests]'); // фильтр гостей
 
   // переменные для хранения текущего значения выбранного фильтра
   // зададим "стартовое" значение
   var selectedTypeValue = selectType.value;
   var selectedPriceValue = selectPrice.value;
+  var selectedRoomValue = selectRoom.value;
+  var selectedGuestValue = selectGuest.value;
 
   var unselectedValue = 'any'; // значение "невыбранного" фильтра
 
@@ -35,7 +39,14 @@
     },
     'housing-price': function (val) {
       selectedPriceValue = val;
+    },
+    'housing-rooms': function (val) {
+      selectedRoomValue = val;
+    },
+    'housing-guests': function (val) {
+      selectedGuestValue = val;
     }
+
   };
 
   // объект-мапа для перевода значений фильтра "Стоимость" в цифры
@@ -84,6 +95,30 @@
     && it.offer.price < priceNumberMap[selectedPriceValue].max;
   };
 
+  /**
+  * сравнивает количество комнат
+  * @param {Object} it
+  * @return {boolean}
+  */
+  var checkRoomValue = function (it) {
+    if (selectedRoomValue === unselectedValue) {
+      return true;
+    }
+    return it.offer.rooms.toString() === selectedRoomValue;
+  };
+
+    /**
+  * сравнивает количество гостей
+  * @param {Object} it
+  * @return {boolean}
+  */
+  var checkGuestValue = function (it) {
+    if (selectedGuestValue === unselectedValue) {
+      return true;
+    }
+    return it.offer.guests.toString() === selectedGuestValue;
+  };
+
 
   // добавим обработчик события change на всю форму,
   // а выбранный фильтр будем отслеживать по evt.target
@@ -93,7 +128,7 @@
 
     // отфильтруем данные
     filteredData = window.data.adsList.filter(function (ad) {
-      return checkTypeValue(ad) && checkPriceValue(ad);
+      return checkTypeValue(ad) && checkPriceValue(ad) && checkRoomValue(ad) && checkGuestValue(ad);
     });
 
     // запустим отрисовку отфильтрованных меток
