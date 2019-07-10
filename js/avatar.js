cca946326e489e73f56e700bb9be3baf75800fb4
Natallia Viazenkina <natala.viaz@gmail.com>
2019-07-10 19:44:33 +0300

    Реализовано добавление аватара перетаскиванием (drag n drop)

--- js/avatar.js
+++ js/avatar.js
@@ -6,6 +6,7 @@
 
   var avatarChooser = window.utils.adForm.querySelector('.ad-form__field input'); // поле загрузки аватарки
   var avatar = window.utils.adForm.querySelector('.ad-form-header__preview img');
+  var dropZone = window.utils.adForm.querySelector('.ad-form-header__drop-zone');
 
   avatarChooser.addEventListener('change', function () {
     var file = avatarChooser.files[0];
@@ -26,5 +27,73 @@
     }
 
   });
+
+  /**
+  * запрещает события по умолчанию
+  * @param {Event} evt
+  */
+  var preventDefaults = function (evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+  };
+
+  /**
+  * изменяет цвет границы
+  */
+  var highlight = function () {
+    dropZone.style.borderColor = '#ff5635';
+  };
+
+  /**
+  * изменяет цвет границы (на первоначальный)
+  */
+  var unhighlight = function () {
+    dropZone.style.borderColor = '#c7c7c7';
+  };
+
+  dropZone.addEventListener('dragenter', function (evt) {
+    preventDefaults(evt);
+    highlight();
+  });
+
+  dropZone.addEventListener('dragover', function (evt) {
+    preventDefaults(evt);
+    highlight();
+    evt.dataTransfer.dropEffect = 'copy';
+  });
+
+  dropZone.addEventListener('dragleave', function (evt) {
+    preventDefaults(evt);
+    unhighlight();
+  });
+
+  dropZone.addEventListener('drop', function (evt) {
+    preventDefaults(evt);
+    unhighlight();
+
+    var dt = evt.dataTransfer;
+    console.log(dt.files);
+    var file = dt.files[0];
+
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        avatar.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+
+
+  });
+
+
 
 })();
